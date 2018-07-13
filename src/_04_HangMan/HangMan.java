3bbb8d53c6991522508a7fe60a841cb8e65e13f7
Haris Sarwary <sarwary.haris@gmail.com>
2018-07-13 14:57:00 -0700

    hangman

--- src/_04_HangMan/HangMan.java
+++ src/_04_HangMan/HangMan.java
@@ -1,7 +1,26 @@
 package _04_HangMan;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Stack;
+
+import javax.swing.JOptionPane;
+
 public class HangMan{
+	static String filename="src/_04_HangMan/dictionary.txt";
+	public static void main(String[] args) {
+	HangMan hang = new HangMan();
+	Utilities u = new Utilities();
+	String Num = JOptionPane.showInputDialog("Choose A Number From 1 To 266");
+	int num = Integer.parseInt(Num);
+	Stack<String> string = new Stack<String>();
+	for (int i = 0; i < num; i++) {
+		string.push(u.readRandomLineFromFile(filename));
+	}
 	
+	}
 	
 	
 }
